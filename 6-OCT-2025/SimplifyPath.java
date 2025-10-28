0ed3ffbc67530e424b94f5ec4e8d2c6619822571
Bhumika Agrawal <bhumikaagrawal1208@gmail.com>
2025-10-28 22:31:19 +0530

    Add files via upload

---
+++ 6-OCT-2025/SimplifyPath.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+class Solution {
+    public String SimplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        for (String c : path.split("/")) {
+            if (c.equals("") || c.equals(".")) {
+   
+                continue;
+            } else if (c.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop(); 
+                }
+            } else {
+                stack.push(c); 
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : stack) {
+            sb.append("/").append(s);
+        }
+
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
